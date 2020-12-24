0c5419fa75983392e14effa627a164de44dc924f
marcus <https://github.com/marcuspure>
2020-12-24 14:32:35 +0800

    thirdcountquantity

--- scripts/main.js
+++ scripts/main.js
@@ -1,114 +1,52 @@
-document.addEventListener('DOMContentLoaded',() => {
-    
-    document.querySelectorAll('.remove-btn').forEach(btn =>{
- // 監聽點擊事件
-        btn.addEventListener('click', setRemoveItemBtn)
-    })
-   
-       document.querySelectorAll('.cart .quantity').forEach(input => {
-          input.addEventListener('change', setQuantity)
+document.addEventListener('DOMContentLoaded',()=>{
+  // 第一步先移除購物車
+  document.querySelectorAll('.remove-btn').forEach(btn =>{
+    // 監聽事件當執行時
+    btn.addEventListener('click',setRemoveItemBtn)
+  })
+  // 第三的監聽事件 foreach= 對於html<class以下所有都執行
+  document.querySelectorAll('.cart .quantity').forEach(input =>{
+    // change= 當input的值有所改變 將執行
+    input.addEventListener('change',setQuantity)
+  })
 
-        }) 
-        
-        document.querySelectorAll('.items .add-item-btn').forEach(btn => {
-            btn.addEventListener('click', setAddItemBtn)
-          })
-        document.querySelector('.empty-cart-btn').addEventListener('click',emptyCart)
-        
+
 })
-    function emptyCart(){
-        document.querySelector('.item-list').innerHTML= ''
-        updateCart()
-    }
 
-
-function setAddItemBtn(e) {
-    const product = e.currentTarget.parentElement.parentElement
-    // 定義商品名稱
-    const productName = product.querySelector('.product-name').innerText
-    // 定義然後抓取價錢
-    const price = product.querySelector('.price').innerText.replace('$','')
-    // items = 三個ＩＴＥＭ
-    const items = document.querySelectorAll('.cart-item')
-    for(let i = 0; i < items.length; i++){
-    const item = items[i]
-    const title = items[i].querySelector('.title').innerText
-    if (title == productName) {
-    //   增加數量
-    item.querySelector('.quantity').value = Number(item.querySelector('.quantity').value) + 1
-    updateCart()
-      return;
-    }
-  }
-
-    // element 創造元素ＨＴＭＬ
-    const row = document.createElement('tr')
-    row.classList.add('cart-item')
-    row.innerHTML = `
-        <td class="title">${productName}</td>
-        <td><input type="number" value="1" class="quantity"></td>
-        <td class="price">${price}</td>
-        <td class="subtotal">${price}</td>
-        <td><button class="remove-item-btn btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button></td>       
-    `
-     const itemList = document.querySelector('.item-list')
-     itemList.appendChild(row)
-
-     row.querySelectorAll('.remove-item-btn').forEach(btn => {
-        btn.addEventListener('click', setRemoveItemBtn)
-      })
-      row.querySelectorAll('.cart .quantity').forEach(input => {
-        input.addEventListener('change', setQuantity)
-      })
-    //   row.querySelector('.remove-item-btn').addEventListener('click', setRemoveItemBtn)也能增加
-
-    //  row.querySelector('.cart .quantity').addEventListener('change', setQuantity)也能加數量
-      updateCart()
-  }
-
-//   計算數量
-function setQuantity(e) {
-    // target =btn
-    const input = e.target
-    let quantity = input.value
-    // 如果數量小於等於０ 
-    if (quantity <= 0){
-        // 數量預設為1
-        quantity = 1;
-        // 要等於1
-    }
-    e.target.value = quantity
-    
-    // 定義cartItem 裡面的值裡面的複層的複層
-    const cartItem = input.parentElement.parentElement
-    const price = cartItem.querySelector('.price').innerText.replace('$','')
-    cartItem.querySelector('.subtotal').innerText = `$${quantity * price}`
-
-    updateCart()
-  }
-
-// 移除購物車
-function setRemoveItemBtn(e) {
+// 移除欄位
+function setRemoveItemBtn(e){
   const row = e.currentTarget.parentElement.parentElement
   row.remove()
-
-  updateCart()
 }
-
-// 算購物車總價
+// 第二步 更新購物車總價
 function updateCart(){
-    const cartItems = document.querySelectorAll('.cart .cart-item')
+  // catitems 對應htmlcalss   <tr class="cart-item">
+  const carItems = document.querySelectorAll('.cart .cart-item')
+  // 初始值為總價0
+  let total = 0
+  carItems.forEach(item =>{
+    // 定義選取數量
+    const quantity = item.querySelector('.quantity').value
+    // 定義選取價錢 並移除錢符號
+    const price = item.querySelector('.price').innerText.replace('$','')
+    item.querySelector('.subtotal').innerText = `$${quantity*price}`
+    // 數量＊價錢
+    total +=(quantity*price)
+  })
+  // 定義選取總價錢
+  document.querySelector('.total-price').innerText=`$${total}`
+}
+// 第三計算數量
+function setQuantity(e){
+  // input<e value值
+  // target 等於觸發事件
+  const input = e.target
 
-    // 讓一開始的總值為0
-    let total = 0
-
-    cartItems.forEach(item =>{
-      const quantity =  item.querySelector('.quantity').value
-      const price =  item.querySelector('.price').innerText.replace('$', '')
-      item.querySelector('.subtotal').innerText = `$${ quantity * price }`
-        total += (quantity * price)
-    })
-
-    document.querySelector('.total-price').innerText = `$${Math.round(total*100) / 100 }`
-    // console.log(total);
+  let quantity = input.value
+  if(quantity <= 0){
+    // 數量不能小於1
+    quantity = 1;
+  }
+  // e參數值＝數量
+  e.target.value = quantity
 }
